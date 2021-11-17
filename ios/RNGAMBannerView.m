dda8a0fff9f5964fce0cf1a1942a4bf652a0e29e
Antonio Miceli <amiceli@torstar.ca>
2021-11-17 15:30:34 -0500

    change to GAMBAnnerView

--- ios/RNGAMBannerView.m
+++ ios/RNGAMBannerView.m
@@ -109,7 +109,7 @@
 
 /// Tells the delegate that an ad request successfully received an ad. The delegate may want to add
 /// the banner view to the view hierarchy if it hasn't been added yet.
-- (void)bannerViewDidReceiveAd:(nonnull GADBannerView *)bannerView
+- (void)bannerViewDidReceiveAd:(nonnull GAMBannerView *)bannerView
 {
     if (self.onSizeChange) {
         self.onSizeChange(@{
@@ -125,7 +125,7 @@
 
 /// Tells the delegate that an ad request failed. The failure is normally due to network
 /// connectivity or ad availablility (i.e., no fill).
-- (void)bannerView:(nonnull GADBannerView *)bannerView
+- (void)bannerView:(nonnull GAMBannerView *)bannerView
     didFailToReceiveAdWithError:(nonnull NSError *)error
 {
     if (self.onAdFailedToLoad) {
@@ -134,7 +134,7 @@
 }
 
 /// Tells the delegate that an impression has been recorded for an ad.
-- (void)bannerViewDidRecordImpression:(nonnull GADBannerView *)bannerView
+- (void)bannerViewDidRecordImpression:(nonnull GAMBannerView *)bannerView
 {
     if (self.onAdRecordImpression) {
         self.onAdRecordImpression(@{});
@@ -143,7 +143,7 @@
 
 /// Tells the delegate that a full screen view will be presented in response to the user clicking on
 /// an ad. The delegate may want to pause animations and time sensitive interactions.
-- (void)bannerViewWillPresentScreen:(nonnull GADBannerView *)bannerView
+- (void)bannerViewWillPresentScreen:(nonnull GAMBannerView *)bannerView
 {
     if (self.onAdOpened) {
         self.onAdOpened(@{});
@@ -151,7 +151,7 @@
 }
 
 /// Tells the delegate that the full screen view will be dismissed.
-- (void)bannerViewWillDismissScreen:(nonnull GADBannerView *)bannerView
+- (void)bannerViewWillDismissScreen:(nonnull GAMBannerView *)bannerView
 {
     if (self.onAdClosed) {
         self.onAdClosed(@{});
